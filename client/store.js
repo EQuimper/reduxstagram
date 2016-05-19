b851344eb6c3666fcea04b61b3905e1411de799c
EQuimper <quimperemanuel@gmail.com>
2016-05-18 21:28:00 -0600

    Setup store

---
+++ client/store.js
@@ -0,0 +1,26 @@
+import {
+	createStore,
+	compose
+} from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Import reducer
+import rootReducer from './reducers/index';
+
+// Import the data
+import comments from './data/comments';
+import posts from './data/posts';
+
+// Create an object for the default data
+const defaultState = {
+	posts,
+	comments
+};
+
+// Create store
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
